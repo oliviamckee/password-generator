6fc600e93b01bf9697674fbbf4adc0084ceb8a2f
Olivia McKee <olivia.mckee97@gmail.com>
2022-05-13 03:06:52 -0500

    array for each criteria option

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,18 @@
 // Assignment code here
 
+//array for each password criteria 
+ var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+ var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; 
+ var numberCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+ var specialCharacters = ['!', '"', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '^', '_', '`', '|', '~'];
+
 //question prompts 
 function generatePassword() {
   var finalPassword = "";
   var lowercase = window.confirm("Would you like to include lowercase letters?");
   var uppercase = window.confirm("Would you like to include uppercase letters?");
   var numbers = window.confirm("Would you like to include numbers?");
-  var specialCharacters = window.confirm("Would you like to include special characters?");
+  var special = window.confirm("Would you like to include special characters?");
   return finalPassword;
 }
 
